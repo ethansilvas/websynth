337d173c4897d0d9644a37a579156c37167b3f44
Sherman <ethansilvas2@gmail.com>
2018-05-17 12:18:56 -0700

    gets the keyboard running on the app

--- src/Components/Keyboard/keyboard.js
+++ src/Components/Keyboard/keyboard.js
@@ -1,5 +1,42 @@
 import React from 'react';
 
-export default function Keyboard() {
+import './Keyboard.css';
+//import './Keyboard.less';
 
+function Keyboard() {
+    return (
+        <div className="keyboard">
+            <ul class="keys">
+                <li class="white c"></li>
+                <li class="black cs"></li>
+                <li class="white d"></li>
+                <li class="black ds"></li>
+                <li class="white e"></li>
+                <li class="white f"></li>
+                <li class="black fs"></li>
+                <li class="white g"></li>
+                <li class="black gs"></li>
+                <li class="white a"></li>
+                <li class="black as"></li>
+                <li class="white b"></li>
+
+                <li class="white c"></li>
+                <li class="black cs"></li>
+                <li class="white d"></li>
+                <li class="black ds"></li>
+                <li class="white e"></li>
+                <li class="white f"></li>
+                <li class="black fs"></li>
+                <li class="white g"></li>
+                <li class="black gs"></li>
+                <li class="white a"></li>
+                <li class="black as"></li>
+                <li class="white b"></li>
+
+                <li class="white c"></li>
+            </ul>
+        </div>
+    )
 }
+
+export default Keyboard;
