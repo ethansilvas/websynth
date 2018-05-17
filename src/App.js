8e9d6041799bf6ed6054c7e45d205c5f65d01ac9
Sherman <ethansilvas2@gmail.com>
2018-05-17 12:38:30 -0700

    fix misc changes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import Keyboard from '../src/Components/Keyboard/Keyboard';
 class App extends Component {
   render() {
     return (
-      <div className="Keyboard">
-        <Keyboard keyboard="keyboard"/>
-      </div>
+      <Keyboard />
     );
   }
 }
