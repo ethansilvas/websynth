2c5422bf9aebb8af5d6899e2c318f0d14d6331db
Jeanette Silvas <jeanette.a.silvas@gmail.com>
2018-05-25 17:02:37 -0700

    tweaks to fix less

---
+++ src/Components/Keyboard/Keyboard.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import "./Keyboard.css";
+//import './Keyboard.less';
+
+function Keyboard() {
+    return (
+        <div className="keyboard">
+            <ul className="keys">
+                <li className="white c" />
+                <li className="black cs" />
+                <li className="white d" />
+                <li className="black ds" />
+                <li className="white e" />
+                <li className="white f" />
+                <li className="black fs" />
+                <li className="white g" />
+                <li className="black gs" />
+                <li className="white a" />
+                <li className="black as" />
+                <li className="white b" />
+
+                <li className="white c" />
+                <li className="black cs" />
+                <li className="white d" />
+                <li className="black ds" />
+                <li className="white e" />
+                <li className="white f" />
+                <li className="black fs" />
+                <li className="white g" />
+                <li className="black gs" />
+                <li className="white a" />
+                <li className="black as" />
+                <li className="white b" />
+
+                <li className="white c" />
+            </ul>
+        </div>
+    );
+}
+
+export default Keyboard;
