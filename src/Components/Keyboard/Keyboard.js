07b7793b0faae6324eaba1a2682a378b7cb95f51
Sherman <ethansilvas2@gmail.com>
2018-05-27 20:51:13 -0700

    add temporary wood panels to resemble Moog Prodigy

--- src/Components/Keyboard/Keyboard.js
+++ src/Components/Keyboard/Keyboard.js
@@ -5,34 +5,40 @@ import "./Keyboard.css";
 function Keyboard() {
     return (
         <div className="keyboard">
-            <ul className="keys">
-                <li className="white c" />
-                <li className="black cs" />
-                <li className="white d" />
-                <li className="black ds" />
-                <li className="white e" />
-                <li className="white f" />
-                <li className="black fs" />
-                <li className="white g" />
-                <li className="black gs" />
-                <li className="white a" />
-                <li className="black as" />
-                <li className="white b" />
+            <ul className="synth"> 
+                <li className="left"></li>
+                <li className="bl"></li>
+                
+                <li className="white c"></li>
+                <li className="black cs"></li>
+                <li className="white d"></li>
+                <li className="black ds"></li>
+                <li className="white e"></li>
+                <li className="white f"></li>
+                <li className="black fs"></li>
+                <li className="white g"></li>
+                <li className="black gs"></li>
+                <li className="white a"></li>
+                <li className="black as"></li>
+                <li className="white b"></li>
 
-                <li className="white c" />
-                <li className="black cs" />
-                <li className="white d" />
-                <li className="black ds" />
-                <li className="white e" />
-                <li className="white f" />
-                <li className="black fs" />
-                <li className="white g" />
-                <li className="black gs" />
-                <li className="white a" />
-                <li className="black as" />
-                <li className="white b" />
+                <li className="white c"></li>
+                <li className="black cs"></li>
+                <li className="white d"></li>
+                <li className="black ds"></li>
+                <li className="white e"></li>
+                <li className="white f"></li>
+                <li className="black fs"></li>
+                <li className="white g"></li>
+                <li className="black gs"></li>
+                <li className="white a"></li>
+                <li className="black as"></li>
+                <li className="white b"></li>
 
-                <li className="white c" />
+                <li className="white c"></li>
+                
+                <li className="br"></li>
+                <li className="right"></li>
             </ul>
         </div>
     );
